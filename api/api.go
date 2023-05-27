9fece5fc38d5b44009b902d5482c956ad4670a42
lianhong <2758988938@qq.com>
2023-05-27 16:23:31 +0800

    add mod

---
+++ api/api.go
@@ -0,0 +1,18 @@
+package api
+
+import ()
+
+const (
+	Referer  = "https://weibo.com/"
+	Shouer   = "https://iw233.cn/api.php?sort=cat&referer"
+	Baisi    = "http://aikohfiosehgairl.fgimax2.fgnwctvip.com/uyfvnuvhgbuiesbrghiuudvbfkllsgdhngvbhsdfklbghdfsjksdhnvfgkhdfkslgvhhrjkdshgnverhbgkrthbklg.php?sort=ergbskjhebrgkjlhkerjsbkbregsbg"
+	Heisi    = "http://aikohfiosehgairl.fgimax2.fgnwctvip.com/uyfvnuvhgbuiesbrghiuudvbfkllsgdhngvbhsdfklbghdfsjksdhnvfgkhdfkslgvhhrjkdshgnverhbgkrthbklg.php?sort=rsetbgsekbjlghelkrabvfgheiv"
+	Siwa     = "http://aikohfiosehgairl.fgimax2.fgnwctvip.com/uyfvnuvhgbuiesbrghiuudvbfkllsgdhngvbhsdfklbghdfsjksdhnvfgkhdfkslgvhhrjkdshgnverhbgkrthbklg.php?sort=dsrgvkbaergfvyagvbkjavfwe"
+	Bizhi    = "https://iw233.cn/api.php?sort=iw233"
+	Baimao   = "https://iw233.cn/api.php?sort=yin"
+	Star     = "https://iw233.cn/api.php?sort=xing"
+	Sese     = "http://aikohfiosehgairl.fgimax2.fgnwctvip.com/uyfvnuvhgbuiesbrghiuudvbfkllsgdhngvbhsdfklbghdfsjksdhnvfgkhdfkslgvhhrjkdshgnverhbgkrthbklg.php?sort=qwuydcuqwgbvwgqefvbwgueahvbfkbegh"
+	Biaoqing = "https://iw233.cn/api.php?sort=img"
+	Cos      = "http://aikohfiosehgairl.fgimax2.fgnwctvip.com/uyfvnuvhgbuiesbrghiuudvbfkllsgdhngvbhsdfklbghdfsjksdhnvfgkhdfkslgvhhrjkdshgnverhbgkrthbklg.php/?sort=cos"
+	Mang     = "https://iw233.cn/api.php?sort=random"
+)
