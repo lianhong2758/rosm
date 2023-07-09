0d227104caf08c3fd217fb56411781874ee614e7
lianhong <2758988938@qq.com>
2023-07-09 19:38:54 +0800

    add web and process

--- web/web.go
+++ web/web.go
@@ -2,11 +2,76 @@ package web
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"strconv"
 )
 
+// 请求快捷包装
+func Web(client *http.Client, url, method string, setheaders func(*http.Request), body io.Reader) (data []byte, err error) {
+	// 提交请求
+	var request *http.Request
+	request, err = http.NewRequest(method, url, body)
+	if err == nil {
+		// 增加header选项
+		setheaders(request)
+		var response *http.Response
+		response, err = client.Do(request)
+		if err != nil {
+			return
+		}
+		if response.StatusCode != http.StatusOK {
+			s := fmt.Sprintf("status code: %d", response.StatusCode)
+			err = errors.New(s)
+			return
+		}
+		data, err = io.ReadAll(response.Body)
+		response.Body.Close()
+	}
+	return
+}
+
+// RequestDataWith 使用自定义请求头获取数据
+func RequestDataWith(client *http.Client, url, method, referer, ua string, body io.Reader) (data []byte, err error) {
+	// 提交请求
+	var request *http.Request
+	request, err = http.NewRequest(method, url, body)
+	if err == nil {
+		// 增加header选项
+		if referer != "" {
+			request.Header.Add("Referer", referer)
+		}
+		if ua != "" {
+			request.Header.Add("User-Agent", ua)
+		}
+		var response *http.Response
+		response, err = client.Do(request)
+		if err == nil {
+			if response.StatusCode != http.StatusOK {
+				s := fmt.Sprintf("status code: %d", response.StatusCode)
+				err = errors.New(s)
+				return
+			}
+			data, err = io.ReadAll(response.Body)
+			response.Body.Close()
+		}
+	}
+	return
+}
+
+// GetRealURL 获取跳转后的链接
+func GetRealURL(url string) (realurl string, err error) {
+	data, err := http.Head(url)
+	if err != nil {
+		return
+	}
+	_ = data.Body.Close()
+	realurl = data.Request.URL.String()
+	return
+}
+
+// 快速获取
 func GetData(url, ua string) (body []byte, err error) {
 	var client = &http.Client{}
 	req, err := http.NewRequest(http.MethodGet, url, nil)
@@ -33,4 +98,8 @@ func GetData(url, ua string) (body []byte, err error) {
 		return nil, err
 	}
 	return
+}
+
+func NewDefaultClient() *http.Client {
+	return &http.Client{}
 }
